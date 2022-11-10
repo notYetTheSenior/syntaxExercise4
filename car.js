fa34ef570fcdcbceca6439de3041a3f3ae5e3eff
Rasul <rasulakhmadullin0@gmail.com>
2022-11-10 18:36:22 +0300

    just in case

---
+++ car.js
@@ -0,0 +1,15 @@
+const carName = 'Porsche 911'
+let maxCarSpeed = 250
+let carOwner = 'Rasul'
+maxCarSpeed = 260
+console.log('Максимальная скорость изменилась', maxCarSpeed)
+maxCarSpeed = 270
+console.log('Максимальная скорость изменилась', maxCarSpeed)
+maxCarSpeed = 300
+console.log('Максимальная скорость изменилась', maxCarSpeed)
+carOwner = 'Aleksey'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Igor'
+console.log('У машины изменился владелец', carOwner)
+carOwner = 'Rasul'
+console.log('У машины изменился владелец', carOwner)
